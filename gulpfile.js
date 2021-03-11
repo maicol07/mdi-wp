e6ebf0a3bf91dd8bc1ea7854a4fbf712ffc58134
Maicol <maicolbattistini@live.it>
2021-03-11 13:42:28 +0100

    chore(deps): Setup dependencies

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021.  Maicol07 - Tutti i diritti riservati - All rights reserved
+ */
+
+import gulp from 'gulp';
+import del from 'del';
+import { zip } from 'gulp-vinyl-zip';
+
+/**
+ * Delete the zip
+ */
+function clean() {
+	return del( 'mdi-wp.zip' );
+}
+
+/**
+ * Pack the plugin
+ */
+function pack() {
+	return gulp.src( [
+		'**/*',
+		'!node_modules/**/*',
+		'!test/**/*',
+		'!vendor/**/*',
+		'!*',
+		'composer.json',
+		'composer.lock',
+		'LICENSE',
+		'package.json',
+		'plugin.php',
+		'README.md',
+		'readme.txt',
+		'yarn.lock',
+	] )
+		.pipe( zip( 'mdi-wp.zip' ) )
+		.pipe( gulp.dest( './' ) );
+}
+
+exports.default = gulp.series( clean, pack );
+exports.clean = clean;
+exports.zip = pack;
